c1ba5e9de3f6af073065f39f08d4a844dd2ea017
Jay R Bolton <jayrbolton@gmail.com>
2018-01-10 13:32:29 -0800

    Add a lot of functionality towards a robust nested chart system

--- index.js
+++ index.js
@@ -1,94 +1,171 @@
+var flattenObj = require('flat')
 
-function Harel (config) {
-  if (!(this instanceof Harel)) return new Harel(config)
-  var accessible = {}
-  var initialCount = 0
-  for (var i = 0; i < config.states.length; ++i) {
-    var name = config.states[i]
-    accessible[name] = false
-    if (config.initial[name]) initialCount += 1
+function Harel (opts) {
+  if (!opts.states || !Array.isArray(opts.states)) {
+    throw new TypeError('Pass in a .states array')
   }
-  if (initialCount === 0) throw new Error('You must set an initial set of states')
+  opts = defaults(opts)
+  for (var i = 0; i < opts.states.length; ++i) {
+    opts.accessibleStates[opts.states[i]] = false
+  }
+  setInitialStates(opts.initial, opts)
+
+  var chart = {
+    nested: {},
+    eventPaths: {},
+    accessibleStates: opts.accessibleStates,
+    states: opts.initial,
+    initial: opts.initial,
+    flatStates: flattenObj(opts.initial),
+    event: function (name) { return event(name, this) }
+  }
+
+  // Initialize nested charts
+  for (var chartName in opts.where) {
+    opts.where[chartName].skipAccessibilityValidation = true
+    chart.nested[chartName] = Harel(opts.where[chartName])
+  }
+
   // `eventPaths` is an object like {EVENT_NAME: {sourceState: 'destinationState'}}
-  var eventPaths = {}
-  for (var eventName in config.events) {
-    eventPaths[eventName] = {}
-    var pairs = config.events[eventName]
+  for (var eventName in opts.events) {
+    chart.eventPaths[eventName] = {}
+    var pairs = opts.events[eventName]
     if (typeof pairs[0] === 'string') pairs = [pairs]
     for (var j = 0; j < pairs.length; ++j) {
       var source = pairs[j][0]
       var dest = pairs[j][1]
-      if (!accessible.hasOwnProperty(source)) throw new Error('Invalid transition from: ' + source)
-      if (!accessible.hasOwnProperty(dest)) throw new Error('Invalid transition to: ' + dest)
-      if (eventPaths[eventName][source]) throw new Error('Ambiguous state transition event "' + eventName + '" from state "' + source + '"')
-      eventPaths[eventName][source] = dest
-      accessible[source] = true
-      accessible[dest] = true
-      // Handle transitions to nested states within nested charts (eg. {JUMP: ['x', 'y', 'z', 'q']})
-      if (pairs[j][2]) {
-        eventPaths[eventName[source]] = {[dest]: pairs[j][2]}
-      }
+      if (chart.eventPaths[eventName][source]) throw new Error('Ambiguous state transition event "' + eventName + '" from state "' + source + '"')
+      // Set nested chart states as accessible
+      var destPath = dest.split('.')
+      setAccessiblePath(destPath, chart)
+      chart.eventPaths[eventName][source] = destPath
     }
   }
   // Check for inaccessible states
-  for (var state in accessible) {
-    if (!accessible[state]) {
+  if (!opts.skipAccessibilityValidation) {
+    validateAccessibility(chart)
+  }
+  return chart
+}
+
+// Set defaults on the options to create a chart
+function defaults (opts) {
+  if (!('events' in opts)) opts.events = {}
+  if (!('initial' in opts)) opts.initial = {}
+  if (!('where' in opts)) opts.where = {}
+  if (!('accessibleStates' in opts)) opts.accessibleStates = {}
+  for (var i = 0; i < opts.states.length; ++i) {
+    opts.accessibleStates[opts.states[i]] = false
+  }
+  for (var nestedName in opts.where) {
+    opts.where[nestedName] = defaults(opts.where[nestedName])
+  }
+  return opts
+}
+
+// If some initial states activate nested charts, then we need to transition those charts
+function setInitialStates (initial, opts) {
+  if (initial === true) return opts.initial
+  for (var stateName in initial) {
+    if (!(stateName in opts.accessibleStates)) {
+      throw new Error('Invalid initial state: ' + stateName + ' not found')
+    }
+    opts.accessibleStates[stateName] = true
+    if (stateName in opts.where) {
+      opts.initial[stateName] = setInitialStates(initial[stateName], opts.where[stateName])
+    } else {
+      opts.initial[stateName] = true
+    }
+  }
+  return opts.initial
+}
+
+// Validate that all states are accessible
+function validateAccessibility (chart) {
+  for (var state in chart.accessibleStates) {
+    if (!chart.accessibleStates[state]) {
       throw new Error('Inaccessible state "' + state + '"')
     }
   }
-  // Handle nested charts
-  var nested = {}
-  /*
-  config.where = config.where || {}
-  for (var chartName in config.where) {
-    if (config.where[chartName]._statechart) {
-      nested[chartName] = Harel(config.where[chartName])
-    } else {
-      nested[chartName] = config.where[chartName]
-    }
+  for (var chartName in chart.nested) {
+    validateAccessibility(chart.nested[chartName])
   }
-  */
-  this.states = config.initial
-  this.eventPaths = eventPaths
-  this._statechart = true
-  this.nested = nested
-  return this
 }
 
-Harel.prototype.event = function (name) { return event(name, this) }
+// Given a path of nested states, like ['x1', 'y1', 'z1']
+// Mark each state as accessible in their respective charts
+function setAccessiblePath (path, chart) {
+  chart.accessibleStates[path[0]] = true
+  var nested = chart.nested[path[0]]
+  if (nested && path.length > 1) {
+    return setAccessiblePath(path.slice(1), nested)
+  }
+}
 
 function event (eventName, chart) {
+  var newChart = Object.assign({}, chart)
+  newChart.nested = Object.assign({}, chart.nested)
+  newChart.states = Object.assign({}, chart.states)
+  // Handle nested event transitions within a nested chart -- eg. "c1.EVENT"
+  var eventScope = eventName.split('.')
+  if (eventScope.length > 1) {
+    var chartName = eventScope[0]
+    var nested = chart.nested[chartName]
+    if (!nested) {
+      throw new Error('Cannot find this nested chart: ' + eventScope[0])
+    }
+    if (!newChart.states[chartName]) {
+      throw new Error('Cannot transition in this inactive nested chart: ' + eventScope[0])
+    }
+    newChart.nested[chartName] = event(eventScope.slice(1).join('.'), nested)
+    newChart.states[chartName] = newChart.nested[chartName].states
+    return newChart
+  }
   var sources = chart.eventPaths[eventName]
-  if (!sources) throw new Error('Unrecognized event: ' + eventName)
-  var newStates = {}
   var transitionCount = 0
-  for (var state in chart.states) {
+  for (var state in chart.flatStates) {
     var dest = sources[state]
     if (dest) {
+      transition(state.split('.'), dest, newChart)
       transitionCount += 1
-      // Handle nested transition to its initial state
-      /*
-      if (chart.nested[dest]) {
-        if (typeof sources[state] === 'object') {
-          var nestedState = sources[state]
-          newStates[dest] = sources[state]
-          newStates[dest] = Object.assign({states: sources[state], newStates[dest]})
-        } else {
-          newStates[dest] = chart.nested[dest].initial
-        }
-      } else {
-      */
-      newStates[dest] = true
-      // }
-    } else {
-      newStates[state] = chart.states[state]
     }
   }
   if (transitionCount === 0) {
     throw new Error('Invalid event "' + eventName + '" from state "' + JSON.stringify(state) + '"')
   }
-  var newChart = Object.assign(chart, {states: newStates})
+  newChart.flatStates = flattenObj(newChart.states)
   return newChart
+}
+
+function transition (sourcePath, destPath, chart) {
+  // Unset the source
+  if (sourcePath) {
+    delete chart.states[sourcePath[0]]
+  }
+  // Set the dest -- possibly recursive
+  var nested = chart.nested[destPath[0]]
+  if (destPath.length === 1) {
+    // Transition into nested initial state
+    if (nested) {
+      nested.states = nested.initial
+      chart.states[destPath[0]] = nested.initial
+      return
+    }
+    // Transition into a normal state
+    chart.states[destPath[0]] = true
+  } else { // gt 1 -- some kind of nested chart path
+    // Transitioning within the same nested chart
+    if (sourcePath && sourcePath[0] === destPath[0]) {
+      transition(sourcePath.slice(1), destPath.slice(1), nested)
+      chart.states[destPath[0]] = nested.states
+      return
+    }
+    // Transitioning into some new nested chart
+    if (nested) {
+      transition(null, destPath.slice(1), nested)
+      chart.states[destPath[0]] = nested.states
+    }
+  }
 }
 
 module.exports = {
--- test/index.js
+++ test/index.js
@@ -43,36 +43,31 @@ test('transitions correctly on loops', t => {
   t.end()
 })
 
-test('it throws an error with a blank initial state', t => {
-  t.throws(() => Chart.create({states: ['s1'], events: {EV: ['s1', 's1']}, initial: {}}))
-  t.end()
-})
-
 test('it throws an error when transition on a valid event that is inaccessible', t => {
   const c1 = Chart.create({states: ['s1', 's2'], events: {LOOP: ['s1', 's1'], MOVE: ['s1', 's2']}, initial: {s2: true}})
   t.throws(() => Chart.event('LOOP', c1))
   t.end()
 })
 
-test.skip('nested charts', t => {
-  const parent = Chart.create({
+test('transition to a nested chart with an initial state', t => {
+  const p1 = Chart.create({
     states: ['s1', 'c1'],
     events: { PUSH: ['s1', 'c1'] },
     initial: {s1: true},
     where: {
       c1: {
-        initial: {a1: true},
+        initial: {a1: true, b1: true},
         states: ['a1', 'b1'],
-        events: {GO: ['a1', 'b1']}
+        events: {}
       }
     }
   })
-  const p2 = parent.event('PUSH')
-  t.deepEqual(p2.states, {c1: {a1: true}})
+  const p2 = p1.event('PUSH')
+  t.deepEqual(p2.states, {c1: {a1: true, b1: true}})
   t.end()
 })
 
-test.skip('nested charts to a specific state, overriding initial', t => {
+test('transition to a nested chart with a specific state', t => {
   const parent = Chart.create({
     states: ['s1', 'c1'],
     events: { PUSH: ['s1', 'c1.b1'] },
@@ -81,11 +76,202 @@ test.skip('nested charts to a specific state, overriding initial', t => {
       c1: {
         initial: {a1: true},
         states: ['a1', 'b1'],
-        events: {GO: ['a1', 'b1']}
+        events: {}
       }
     }
   })
   const p2 = parent.event('PUSH')
-  t.deepEqual(p2.states, {c1: {b1: true}})
+  t.deepEqual(p2.states, {c1: {a1: true, b1: true}})
   t.end()
 })
+
+test('transition within the same nested chart from the parent', t => {
+  const p1 = Chart.create({
+    states: ['c1'],
+    events: { LOOP: [['c1.a1', 'c1.b1'], ['c1.b1', 'c1.a1']] },
+    initial: {c1: {a1: true}},
+    where: {
+      c1: {
+        initial: {a1: true},
+        states: ['a1', 'b1']
+      }
+    }
+  })
+  t.deepEqual(p1.states, {c1: {a1: true}})
+  const p2 = p1.event('LOOP')
+  t.deepEqual(p2.states, {c1: {b1: true}})
+  const p3 = p2.event('LOOP')
+  t.deepEqual(p3.states, {c1: {a1: true}})
+  t.end()
+})
+
+test('transition within the same nested chart from a nested event', t => {
+  const p1 = Chart.create({
+    states: ['c1'],
+    initial: {c1: {a1: true}},
+    where: {
+      c1: {
+        initial: {a1: true},
+        events: {'LOOP': [['a1', 'b1'], ['b1', 'a1']]},
+        states: ['a1', 'b1']
+      }
+    }
+  })
+  t.deepEqual(p1.states, {c1: {a1: true}})
+  const p2 = p1.event('c1.LOOP')
+  t.deepEqual(p2.states, {c1: {b1: true}})
+  const p3 = p2.event('c1.LOOP')
+  t.deepEqual(p3.states, {c1: {a1: true}})
+  t.end()
+})
+
+test('initially setting a nested chart sets its initial state automatically', t => {
+  const p1 = Chart.create({
+    states: ['c1'],
+    initial: {c1: true},
+    where: {
+      c1: {
+        states: ['a1'],
+        initial: {a1: true}
+      }
+    }
+  })
+  t.deepEqual(p1.states, {c1: {a1: true}})
+  t.end()
+})
+
+test("initially setting a nested chart states the nested chart's state", t => {
+  const p1 = Chart.create({
+    states: ['c1'],
+    initial: {c1: {c2: {b1: true, c3: true}}},
+    where: {
+      c1: {
+        initial: {a1: true},
+        states: ['a1', 'c2'],
+        where: {
+          c2: {
+            initial: {a1: true},
+            states: ['a1', 'b1', 'c3'],
+            where: {
+              c3: {
+                initial: {a1: true},
+                states: ['a1']
+              }
+            }
+          }
+        }
+      }
+    }
+  })
+  t.deepEqual(p1.states, {c1: {a1: true, c2: {a1: true, b1: true, c3: {a1: true}}}})
+  t.deepEqual(p1.nested.c1.states, {a1: true, c2: {a1: true, b1: true, c3: {a1: true}}})
+  t.deepEqual(p1.nested.c1.nested.c2.states, {a1: true, b1: true, c3: {a1: true}})
+  t.deepEqual(p1.nested.c1.nested.c2.nested.c3.states, {a1: true})
+  t.end()
+})
+
+test("nested charts: transition to a double-nested chart's initial state", t => {
+  const parent = Chart.create({
+    states: ['s1', 'c1'],
+    events: { PUSH: ['s1', 'c1.c2'] },
+    initial: {s1: true},
+    where: {
+      c1: {
+        initial: {a1: true},
+        states: ['a1', 'c2'],
+        events: {},
+        where: {
+          c2: {
+            initial: {g1: true},
+            states: ['g1'],
+            events: {}
+          }
+        }
+      }
+    }
+  })
+  const p2 = parent.event('PUSH')
+  t.deepEqual(p2.states, {c1: {a1: true, c2: {g1: true}}})
+  t.end()
+})
+
+test("nested charts: transition to a double-nested chart's specific state", t => {
+  const parent = Chart.create({
+    states: ['s1', 'c1'],
+    events: { PUSH: ['s1', 'c1.c2.h1'] },
+    initial: {s1: true},
+    where: {
+      c1: {
+        initial: {a1: true},
+        states: ['a1', 'c2'],
+        events: {},
+        where: {
+          c2: {
+            initial: {g1: true},
+            states: ['g1', 'h1'],
+            events: {}
+          }
+        }
+      }
+    }
+  })
+  const p2 = parent.event('PUSH')
+  t.deepEqual(p2.states, {c1: {a1: true, c2: {g1: true, h1: true}}})
+  t.end()
+})
+
+test('nested charts: transition from a nested state to another nested state', t => {
+  const parent = Chart.create({
+    states: ['c1', 'c2'],
+    events: { TRANS: ['c1.a1', 'c2.a1'] },
+    initial: {c1: {a1: true}},
+    where: {
+      c1: {
+        initial: {a1: true},
+        states: ['a1'],
+        events: {}
+      },
+      c2: {
+        initial: {a1: true},
+        states: ['a1'],
+        events: {}
+      }
+    }
+  })
+  const p2 = parent.event('TRANS')
+  t.deepEqual(p2.states, {c2: {a1: true}})
+  t.end()
+})
+
+test.skip('nested charts: transition into historical nested state', t => {
+  const p1 = Chart.create({
+    states: ['a1', 'c1'],
+    events: {
+      PUSH: ['a1', 'c1.history'],
+      POP: ['c1.b1', 'a1']
+    },
+    initial: {a1: true},
+    where: {
+      c1: {
+        initial: {a1: true},
+        states: ['a1', 'b1'],
+        events: {LOOP: [['a1', 'b1'], ['b1', 'a1']]}
+      }
+    }
+  })
+  const p2 = p1.event('PUSH')
+  t.deepEqual(p2.states, {c1: {a1: true}}, 'initial push goes to nested initial states')
+  const p3 = p2.event('c1.LOOP')
+  t.deepEqual(p3.states, {c1: {b1: true}})
+  const p4 = p2.event('POP')
+  t.deepEqual(p4.states, {a1: true})
+  const p5 = p2.event('PUSH')
+  t.deepEqual(p5.states, {c1: {b1: true}})
+  t.end()
+})
+
+// TODO
+// - transition into chart history
+// - loop on nested chart, no specific inner states
+// - exit from specific nested chart state
+// - exit from any nested chart state
